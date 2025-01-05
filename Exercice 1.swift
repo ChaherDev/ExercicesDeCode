ef3c8b759b01c685c5f905fb477827f679acb8fc
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-05 10:56:08 +0100

    Exercise 1 resolved

--- Exercice 1.swift
+++ Exercice 1.swift
@@ -1,5 +1,9 @@
-for i in 0...9 {
-    for j in 0...9 {
-        print("\(i)\(j)", terminator: ", ")
+for i in 0...8 {
+    for j in 1...9 {
+        if i > j {
+            continue
+        } else {
+            print("\(i)\(j)", terminator: ", ")
+        }
     }
 }
