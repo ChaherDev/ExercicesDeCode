c09e6bd1b187e7e9ad1772b983242c8f73c4c9d9
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-09 13:16:34 +0100

    Exercise 6 completed

---
+++ Exercice 6.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+for i in 0...98 {
+    for j in i+1...99 {
+        let formattedI = String(format: "%02d", i)
+        let formattedJ = String(format: "%02d", j)
+        print("\(formattedI) \(formattedJ)", terminator: ", ")
+    }
+}
+print("99 99")
