87b9148e0867df359954229526f250002eb8b2e3
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-16 20:23:06 +0100

    Exercice 7.swift completed

--- Exercice 7.swift
+++ Exercice 7.swift
@@ -3,10 +3,13 @@ import Foundation
 for i in 0...98 {
     for j in i+1...99 {
         if i < j {
-            let formattedI = String(format: "%02d", i)
-            let formattedJ = String(format: "%02d", j)
-            print("\(formattedI) \(formattedJ)", terminator: ", ")
+            if i == 98 && j == 99 {
+                print("98 99")
+            } else {
+                let formattedI = String(format: "%02d", i)
+                let formattedJ = String(format: "%02d", j)
+                print("\(formattedI) \(formattedJ)", terminator: ", ")
+            }
         }
     }
 }
-print("99 99")
