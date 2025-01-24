14c6f078c856bb9a98c2d7465bac56e95a5eee80
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-24 10:24:53 +0100

    Exercise 10 completed

--- Exercice 10.swift
+++ Exercice 10.swift
@@ -1,33 +1,37 @@
 var firstNumber = 0
 var secoundNumber = 1
 var thirdNumber = 0
-var initialTerm = 1
+var initialTerm = 0
 var terminalTerm = 0
 
 if CommandLine.arguments.count != 2 {
-    print("Vous devez fournir un argument.")
+    print("Erreur: Vous devez fournir un argument.")
 } else if Int(CommandLine.arguments[1]) != nil {
+    if Int(CommandLine.arguments[1]) == 0 {
+        print("Erreur: comment voulez-vous que je te donne le 0ème élément de la suite de Fibonacci ?")
+    }
     if Int(CommandLine.arguments[1])! < 0 {
-        print("Votre nombre entier doit être positif.")
+        print("Erreur: l’index doit être un entier positif.")
     } else {
         terminalTerm = Int(CommandLine.arguments[1])!
         printTheTerm()
     }
 } else {
-    print("Vous devez fournir un nombre entier.")
+    print("Erreur: vous devez fournir un nombre entier.")
 }
 
 func printTheTerm() {
     func fibonnacySequence() {
     thirdNumber = firstNumber + secoundNumber
     initialTerm += 1
+    if initialTerm == terminalTerm {
+        print(firstNumber)
+    }
     firstNumber = secoundNumber
     secoundNumber = thirdNumber
     }
 
-    while initialTerm < terminalTerm  {
+    while initialTerm != terminalTerm  {
         fibonnacySequence()
     }
-
-    print(secoundNumber)
 }
