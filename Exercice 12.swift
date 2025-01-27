69e6732588a7903d7cb66658ba0cfb2a08a0e74c
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-27 15:15:06 +0100

    Exercice 12.swift completed

--- Exercice 12.swift
+++ Exercice 12.swift
@@ -1,5 +1,26 @@
 func primeNumbers() {
-    
+    let argument = Int(CommandLine.arguments[1]) ?? 0
+
+    var result = [String]()
+
+    for i in 0...argument {
+        if isPrimeNumber(i) {
+            result.append("\(i)")
+        }
+    }
+
+    print(result.joined(separator: ", "))
+}
+
+func isPrimeNumber(_ number: Int) -> Bool {
+    if number == 0 || number == 1 {
+        return false
+    }
+
+    if number == 2 || number == 3 {
+        return true
+    }
+
     let limit = Int(Double(number).squareRoot())
     for diviseur in 2...limit {
         if number % diviseur == 0 {
@@ -8,4 +29,14 @@ func primeNumbers() {
     }
 
     return true
+}
+
+if CommandLine.arguments.count != 2 {
+    print("Vous devez fournir un argument.")
+} else if Int(CommandLine.arguments[1]) == nil {
+    print("Vous devez fournir un nombre entier")
+} else if Int(CommandLine.arguments[1])! < 0 {
+    print("Vous devez fournir un nombre entier positif")
+} else {
+    primeNumbers()
 }
